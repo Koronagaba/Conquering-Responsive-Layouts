bba113550fc1014492d19b662a4e320148cb7464
koronagaba <killleon007@gmail.com>
2022-06-08 18:03:32 +0200

    Day_1

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <div class="container">
+      <div class="intro-content">
+        <h1>Lorem ipsum dolor sit.</h1>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quod unde
+          rerum, deleniti ea obcaecati sint hic odit dicta tenetur qui ut
+          dolorum provident sit, atque, reprehenderit nulla voluptate! Officiis,
+          consectetur?
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <p>
+          Iste ipsa enim delectus porro, ullam repellendus maiores quis rem
+          debitis cum, necessitatibus architecto dolor? Velit, ad quaerat
+          blanditiis veritatis expedita totam vel voluptatem officiis officia ab
+          modi voluptatibus obcaecati.
+        </p>
+        <p>
+          Accusantium minima iusto nobis fuga hic explicabo unde illum,
+          perferendis et animi aperiam quaerat, eaque deleniti alias blanditiis
+          exercitationem commodi repudiandae ullam consequatur incidunt
+          reiciendis repellat officia laboriosam. Esse, modi.
+        </p>
+        <p>
+          Expedita cupiditate iure odit, delectus placeat optio magnam assumenda
+          mollitia aspernatur at saepe nisi commodi natus excepturi voluptate.
+          Recusandae nisi dolorem, necessitatibus optio aliquam repellat.
+          Adipisci, incidunt. Consequuntur, natus nulla.
+        </p>
+      </div>
     </div>
   );
 }
